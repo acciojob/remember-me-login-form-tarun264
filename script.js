8f8bf584e684089dba872d28eb28a36ae2a3e73b
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-10-28 01:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById("login-form").addEventListener("submit",function (event) {
+	event.preventDefault();
+const username=document.getElementById("Username").value;
+const password=document.getElementById("Password").value;
+	const remember=document.getElementById("rememberMe").checked;
+
+	if(remember){
+		localStorage.setItem("username",username);
+		localStorage.setItem("password",password);
+	}
+	else{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+	}
+});
